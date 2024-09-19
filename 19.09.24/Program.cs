c42c47afab3df514570416bb00dc0e42de52569d
MelaNaSpella Dev <sh3p4rdz1k@gmail.com>
2024-09-19 16:09:57 +0200

    bumbum

--- 19.09.24/Program.cs
+++ 19.09.24/Program.cs
@@ -10,6 +10,18 @@ namespace _19._09._24
     {
         static void Main(string[] args)
         {
+            int[] zmienna = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                zmienna[i] = i;
+                Console.WriteLine(zmienna[i].ToString());
+                //System.Diagnostics.Debug.WriteLine("jestem w pętli");
+            }
+            Console.WriteLine("podaj imie");
+            string imie = Console.ReadLine();
+            int a = Console.Read();
+            Console.write
+            Console.ReadKey();
         }
     }
 }
