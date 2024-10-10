a152c0dfa5a90c989f685a02a46882b58a85fe90
MelaNaSpella Dev <sh3p4rdz1k@gmail.com>
2024-10-10 16:48:35 +0200

    sd

---
+++ 10.10.24/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lekcja programowania");
+            var Ini = new ConfigFile(@".\Config.ini");
+            string wartosc_parametru;
+            wartosc_parametru = Ini.Read("IpAddress", "Drukarka");
+            Console.WriteLine("ip address=" + wartosc_parametru);
+            wartosc_parametru = Ini.Read("port", "Drukarka");
+            Console.WriteLine("port=" + wartosc_parametru);
+            string nazwa_drukarki;
+            wartosc_parametru = Ini.Read("nazwa_drukarki", "Drukarka");
+            if (string.IsNullOrEmpty(wartosc_parametru))
+            {
+                nazwa_drukarki = "default_name";
+                Ini.Write("nazwa_drukarki", nazwa_drukarki, "Drukarka");
+            }
+            else if(!string.IsNullOrEmpty(wartosc_parametru))
+            {
+                nazwa_drukarki = wartosc_parametru;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 10.10.24/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        enum typ_skrzyni
+        {
+            manualna = 1,
+            automatyczna = 2
+        }
+        enum dni_tygodnia
+        {
+            poniedzialek=1,
+            wtorek = 2,
+            sroda = 3,
+            czwartek = 4,
+            piatek = 5,
+            sobota = 6,
+            niedziela = 7
+        }
+        static void Main(string[] args)
+        {
+            
+            int skrzynia_biegow;
+            skrzynia_biegow = 1;
+            if (skrzynia_biegow == 1)
+            {
+                Console.WriteLine("skrzynia manualna");
+            }
+            else if(skrzynia_biegow==2)
+            {
+                Console.WriteLine("skrzynia automatyczna");
+            }
+            typ_skrzyni typ_Skrzyni = typ_skrzyni.automatyczna;
+            if (skrzynia_biegow == (int)typ_skrzyni.manualna)
+            {
+                Console.WriteLine("skrzynia manualna");
+            }
+            else if (skrzynia_biegow == (int)typ_skrzyni.automatyczna)
+            {
+                Console.WriteLine("skrzynia automatyczna");
+            }
+            int liczba_dni;
+            liczba_dni = 5+(int)dni_tygodnia.czwartek;
+
+            Console.ReadLine();
+        }
+    }
+}
