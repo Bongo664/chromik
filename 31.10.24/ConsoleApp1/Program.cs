4bd549aea35e34bcf9d30adf2fa2dda1f6761f5e
MelaNaSpella Dev <sh3p4rdz1k@gmail.com>
2024-10-31 16:47:44 +0100

    df

---
+++ 31.10.24/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Zapisz_do_pliku("3CT - 2024");
+            program.Zapisz_do_pliku("3CT");
+            string linia=program.Odczytaj_z_pliku();
+        }
+
+        private void Zapisz_do_pliku(string text)
+        {
+            string path = @".\Plik.txt";
+            StreamWriter zapisz = new StreamWriter(path, true);
+            try
+            {
+                zapisz.WriteLine(text);
+                zapisz.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                zapisz.Close();
+            }
+        }
+
+        private void Zapisz_do_pliku_using(string text)
+        {
+            string path = @".\Plik2.txt";
+            using (StreamWriter zapisz = new StreamWriter(path, true))
+            {
+                zapisz.WriteLine(text);
+            }
+        }
+        private string Odczytaj_z_pliku()
+        {
+            string text = "";
+            using (StreamReader odczytaj = new StreamReader(@".\Plik.txt"))
+            {
+                text = odczytaj.ReadLine();
+                return text;
+            }
+        }
+    }
+}
