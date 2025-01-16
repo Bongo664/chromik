3f33eb6f650e450559fe92ded26e8b10024080c1
MelaNaSpella Dev <sh3p4rdz1k@gmail.com>
2025-01-16 13:47:41 +0100

    gio

---
+++ 16.01.25/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie, nazwisko, nrTelefonu, adresEmail;
+            int? stanKonta = null; //zmienna nullable
+            bool czyPosiadaKonto;
+            if (stanKonta == 0)
+            {
+                Console.WriteLine("Nie posiada konta");
+            }
+            else
+            {
+                Console.WriteLine("stan konta: " + stanKonta.ToString());
+                //uzywanie interpolacji
+                Console.WriteLine($"stan konta: {stanKonta}");
+            }
+            double? zmienna1 = null;
+            double? zmienna2 = 100;
+            double? zmienna3;
+            zmienna3 = zmienna1 ?? zmienna2;
+            Console.WriteLine($"Wartość zmiennej zmienna3 to {zmienna3}");
+            Console.ReadKey();
+        }
+    }
+}
